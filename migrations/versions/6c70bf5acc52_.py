b7c7da6dcf0f918eee6c86174f35e2791ef37439
Suresh Babu Dhanaraj <suresh.dhanaraj@zooplus.com>
2024-01-30 09:56:18 +0100

    migration for db

---
+++ migrations/versions/6c70bf5acc52_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6c70bf5acc52
+Revises: 
+Create Date: 2024-01-30 09:55:27.327453
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6c70bf5acc52'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
